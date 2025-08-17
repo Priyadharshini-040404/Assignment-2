9854de1ee5c3229b9048269d6f20ad8de1614a71
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-17 20:50:17 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+    string choice;
+    char power;
+    bool powerOn = false;
+ 
+    cout << "=== Coffee Machine Program ===" << endl;
+ 
+    // Start
+    cout << "Press 'Y' to Power On: ";
+    cin >> power;
+ 
+    if (power == 'Y' || power == 'y') {
+        powerOn = true;
+        cout << "\nMachine Powered ON ✅" << endl;
+ 
+    }
+    cout << "\n=== Program Ended ===" << endl;
+    return 0;
